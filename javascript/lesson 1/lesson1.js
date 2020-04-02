c1d22cd97b26677fa632b11742b740a3d0ea45a1
Akyl <unsmiling1337@gmail.com>
2020-04-02 23:09:32 +0600

    first commit

---
+++ javascript/lesson 1/lesson1.js
@@ -0,0 +1,26 @@
+
+
+var leftBorderWidht = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+
+console.log(4/0);
+console.log('string'*9);
+
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: "false"
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bpm'];
+
+console.log(arr[0]);
---
+++ javascript/lesson 2/lesson2.js
@@ -0,0 +1,59 @@
+// ' use strict';
+
+a = 5;
+console.log(a);
+
+var leftBorderWidht = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+
+console.log(4/0);
+console.log('string'*9);
+
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: "false"
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bpm'];
+
+console.log(arr[0]);
+
+// alert("Hello World");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log(4 + +" - object");
+
+
+let incr = 10,
+    decr = 10;
+
+//  incr++;
+//  decr--;
+ 
+ console.log(incr++);
+ console.log(decr--);
+
+ console.log(5%2);
+ console.log("2" === 2);
+
+ let isChecked = false,
+     itClose = false;
+
+ console.log(isChecked || !itClose);    
